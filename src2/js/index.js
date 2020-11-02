a68ee32695a2b807f818adb6e880b8660941fe72
Christiano0407 <placebo0704@gmail.com>
2020-11-02 09:12:57 -0600

    callback

---
+++ src2/js/index.js
@@ -0,0 +1,22 @@
+//Callback es una función que, le pasamos como paraámetro otra función.
+/* function sum(num1, num2) {
+    return num1 + num2 ;
+}
+
+function calc (num1, num2, Callback) {
+    return Callback(num1, num2);
+}
+console.log(calc(5, 3, sum));
+
+//Segundo
+function date(callback) {
+    console.log(new Date);
+    setTimeout( function() {
+    let date = new Date;
+    callback(date)
+    }, 3000)
+}
+function primeDate(dateNow) {
+console.log(dateNow)
+}
+date(primeDate); */
