de248af840a9177824eb0d55f92e9478fdf466fc
Christiano0407 <placebo0704@gmail.com>
2020-11-03 09:09:57 -0600

    fetch-async

---
+++ src2/async/challengeTwo.js
@@ -0,0 +1,21 @@
+//CLAVE PARA REALIZAR PETICIONES. 
+const fetchData = require(`../utils/fetchData`);
+const API = `https://rickandmortyapi.com/api/character/`;
+
+//Request
+const anotherFunction = async (url_api) => {
+    try {
+     const data = await fetchData(url_api);
+     const character = await fetchData(`${API}${data.results[0].id}`);
+     const origin = await fetchData(character.origin.url);
+
+     console.log(data.info.count);
+     console.log(character.name);
+     console.log(origin.dimension);
+
+    }catch (error) {
+    console.error(error);
+    }
+}
+
+anotherFunction(API);
